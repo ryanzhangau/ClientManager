708194be858c6bb50ff961f5b5e2fe015b1eb82b
RyanZhang <ryanzhangau@gmail.com>
2017-10-29 06:10:35 +1100

    restructure projects

---
+++ src/container/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import './styles/App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+
+        </header>
+        <section className="body">
+          <aside className="nav"></aside>
+        </section>
+      </div>
+    );
+  }
+}
+
+export default App;
