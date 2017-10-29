a062417561f0c371919bb3ad5297300619cb6ec3
RyanZhang <ryanzhangau@gmail.com>
2017-10-29 21:19:34 +1100

    login style

--- src/components/loginComponent.js
+++ src/components/loginComponent.js
@@ -44,37 +44,39 @@ export default class Login extends React.Component {
 
   render() {
     return (
-      <form>
-        <div className="input-group">
-          <div className="input-group-label">
-            <i className="fa fa-fw fa-user"></i>
+      <div className="login-wrap">
+        <form className='login-form'>
+          <div className="input-group">
+            <div className="input-group-label">
+              <i className="fa fa-fw fa-user"></i>
+            </div>
+            <input type="text" className="input-group-field" placeholder="Username"
+                   value={this.state.user.name}
+                   onChange={this.setUserName.bind(this)}
+            />
           </div>
-          <input type="text" className="input-group-field" placeholder="Username"
-                 value={this.state.user.name}
-                 onChange={this.setUserName.bind(this)}
-          />
-        </div>
-        <div className="input-group">
-          <div className="input-group-label">
-            <i className="fa fa-fw fa-lock"></i>
+          <div className="input-group">
+            <div className="input-group-label">
+              <i className="fa fa-fw fa-lock"></i>
+            </div>
+            <input type="password" className="input-group-field" placeholder="Password"
+                   value={this.state.user.password}
+                   onChange={this.setUserPassword.bind(this)}/>
           </div>
-          <input type="password" className="input-group-field" placeholder="Password"
-                 value={this.state.user.password}
-                 onChange={this.setUserPassword.bind(this)}/>
-        </div>
-        <div className="row">
-          <div className="columns medium-6">
-            <button className="button small login"
-                    onClick={this.loginAsCustomer.bind(this)}
-            >Customer Login</button>
+          <div className="row">
+            <div className="columns medium-6">
+              <button className="button small login"
+                      onClick={this.loginAsCustomer.bind(this)}
+              >Customer Login</button>
+            </div>
+            <div className="columns medium-6">
+              <button className="button small login"
+                      onClick={this.loginAsAdmin.bind(this)}
+              >Admin Login</button>
+            </div>
           </div>
-          <div className="columns medium-6">
-            <button className="button small login"
-                    onClick={this.loginAsAdmin.bind(this)}
-            >Admin Login</button>
-          </div>
-        </div>
-      </form>
+        </form>
+      </div>
     )
   };
 }
