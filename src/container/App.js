78cb54cc21ffef4c27b89c4cdfc1f4c613297270
RyanZhang <ryanzhangau@gmail.com>
2017-10-29 21:19:55 +1100

    added router

--- src/container/App.js
+++ src/container/App.js
@@ -2,13 +2,17 @@ import React, { Component } from 'react';
 import '../styles/App.css';
 import 'font-awesome/css/font-awesome.css';
 import '../styles/login.css';
+import { Route } from 'react-router-dom';
 import Login from '../components/loginComponent';
+import CustomerInfo from '../components/CustomerInfoComponent';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Login/>
+        <Route exact path='/' component={Login} />
+        <Route exact path='/login' component={Login} />
+        <Route exact path='/customer' component={CustomerInfo} />
       </div>
     );
   }
