d3e2086fa50671ae1d6be0b05f6e3e14fab53304
RyanZhang <ryanzhangau@gmail.com>
2017-10-29 13:25:50 +1100

    login logic

--- src/components/loginComponent.js
+++ src/components/loginComponent.js
@@ -1,6 +1,47 @@
 import React from 'react';
 
 export default class Login extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      users: [
+        {name: 'ryan@anctech.com.au', password: '123456', type: 'admin,customer'},
+        {name: 'julie@raywhite.com.au', password: '123456', type: 'customer'},
+      ],
+      user: {
+        name: '',
+        password: '',
+      }
+    }
+  }
+
+  setUserName (e) {
+    this.setState({user: { ...this.state.user, name: e.target.value }});
+  }
+
+  setUserPassword (e) {
+    this.setState({user: { ...this.state.user, password: e.target.value }});
+  }
+
+  loginAsCustomer (e) {
+    e.preventDefault();
+    const matchedUsers = this.state.users.filter(u => u.name === this.state.user.name && u.password === this.state.user.password);
+    if (matchedUsers[0] && matchedUsers[0].type.indexOf('customer') > -1){
+      console.log('Login as Customer');
+    }else
+      console.log('Not logged in');
+  }
+
+  loginAsAdmin (e) {
+    e.preventDefault();
+    const matchedUsers = this.state.users.filter(u => u.name === this.state.user.name && u.password === this.state.user.password);
+    if (matchedUsers[0] && matchedUsers[0].type.indexOf('admin') > -1){
+      console.log('Login as Admin');
+    }else
+      console.log('Not logged in');
+  }
+
   render() {
     return (
       <form>
@@ -8,20 +49,29 @@ export default class Login extends React.Component {
           <div className="input-group-label">
             <i className="fa fa-fw fa-user"></i>
           </div>
-          <input type="text" className="input-group-field" placeholder="Username" />
+          <input type="text" className="input-group-field" placeholder="Username"
+                 value={this.state.user.name}
+                 onChange={this.setUserName.bind(this)}
+          />
         </div>
         <div className="input-group">
           <div className="input-group-label">
             <i className="fa fa-fw fa-lock"></i>
           </div>
-          <input type="text" className="input-group-field" placeholder="Password"/>
+          <input type="password" className="input-group-field" placeholder="Password"
+                 value={this.state.user.password}
+                 onChange={this.setUserPassword.bind(this)}/>
         </div>
         <div className="row">
           <div className="columns medium-6">
-            <button className="button small login">Customer Login</button>
+            <button className="button small login"
+                    onClick={this.loginAsCustomer.bind(this)}
+            >Customer Login</button>
           </div>
           <div className="columns medium-6">
-            <button className="button small login">Admin Login</button>
+            <button className="button small login"
+                    onClick={this.loginAsAdmin.bind(this)}
+            >Admin Login</button>
           </div>
         </div>
       </form>
